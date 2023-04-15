3fd80d83fd9cad9732fbbfa8b86817ab90ed9a8d
AJAYCH <91587077+Ajaych7@users.noreply.github.com>
2023-04-16 00:12:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let employees = [];
-    
+
 const form = document.querySelector('#employee-form');
 const employeeList = document.querySelector('#employee-list');
 const addEmployeeBtn = document.querySelector('#add-employee');
-const employeeCount = document.querySelector('#employee-count');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -34,23 +33,15 @@ form.addEventListener('submit', (event) => {
         form.appendChild(success);
 
         form.reset();
-
-        // Update employee count
-        employeeCount.textContent = `You have ${employees.length} Employees.`;
     }
 });
 
 function addEmployeeToList(employee) {
     const li = document.createElement('li');
     li.id = `employee-${employee.id}`;
-    li.innerHTML = `${employee.name}, ${employee.profession}, ${employee.age} <button class="delete-employee" data-id="${employee.id}">Delete User</button>`;
+    li.innerHTML = `${employee.name}, ${employee.profession}, ${employee.age} <button class="delete-employee" data-id="${employee.id}">Delete</button>`;
     employeeList.appendChild(li);
 }
-
-employees.push(employee);
-const updatedCount = employees.length;
-countElement.textContent = `Updated Employee Count: ${updatedCount}`; 
-
 
 employeeList.addEventListener('click', (event) => {
     if (event.target.classList.contains('delete-employee')) {
@@ -61,12 +52,6 @@ employeeList.addEventListener('click', (event) => {
             employees.splice(index, 1);
             const li = document.querySelector(`#employee-${id}`);
             li.remove();
-
-            // Update employee count
-            employeeCount.textContent = `You have ${employees.length} Employees.`;
-         }
+        }
     }
 });
-
-// Initial employee count
-employeeCount.textContent = 'You have 0 Employees.';
